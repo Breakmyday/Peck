d2eacc0ead961b20b950fa66ebe1045995ca8804
BreakMyDay <hcollierscott@gmail.com>
2023-04-01 10:14:54 -0400

    Refactored

--- Send-KeyPeck.ps1
+++ Send-KeyPeck.ps1
@@ -1,29 +1,22 @@
 function Send-KeyPeck {
     param (
         [string]$Text,
-        [int]$TypeDelay = 50,
+        [int]$TypeDelay  = 50,
         [int]$StartDelay = 3,
         [switch]$Loop,
         [switch]$EnterLine
     )
 
     # Replace &nbsp; with regular spaces
-    $Text = $Text -replace " ", " "
+    $Text = $Text.Replace(" ", " ")
 
- 
-
-    
-    for ($StartDelay; $StartDelay -ge 1; $StartDelay--) {
-        Write-Host $StartDelay -ForegroundColor Yellow
+    # Countdown before starting
+    1..$StartDelay | ForEach-Object {
+        Write-Host $_ -ForegroundColor Yellow
         Start-Sleep -Seconds 1
     }
-    Start-Sleep -Seconds $StartDelay
-
- 
 
     $characters = $Text.ToCharArray()
-
- 
 
     # Load the System.Windows.Forms and user32.dll assemblies
     Add-Type -AssemblyName System.Windows.Forms
@@ -32,13 +25,10 @@ function Send-KeyPeck {
         public static extern short GetAsyncKeyState(int virtualKeyCode);
 "@
 
- 
-
-    while ($true) {
+    do {
         foreach ($char in $characters) {
             # Check whether the interrupt key is pressed
-            $interruptKeyState = $user32::GetAsyncKeyState([System.Windows.Forms.Keys]::'Escape')
-            if ($interruptKeyState -lt 0) {
+            if ($user32::GetAsyncKeyState([System.Windows.Forms.Keys]::'Escape') -lt 0) {
                 Write-Verbose "Interrupt key Escape pressed. Stopping SendKeys operation." -Verbose
                 return
             }
@@ -46,20 +36,10 @@ function Send-KeyPeck {
             Start-Sleep -Milliseconds $TypeDelay
         }
 
- 
-
         if ($EnterLine) {
             Write-Verbose "Pressing enter."
             [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
             Start-Sleep -Milliseconds $TypeDelay
         }
-
- 
-
-        if (-not $Loop) {
-            break
-        }
-    }
+    } while ($Loop)
 }
-
-
